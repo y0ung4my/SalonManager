8ba8e3d2af6e04b29794930628018f23669549f1
Amy Young <youngamy1223@gmail.com>
2022-03-12 15:04:02 -0700

    add Stylist Index

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+
+  }
+}
